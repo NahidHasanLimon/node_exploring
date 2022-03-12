28128de6e194370c1321fb95c9f70ea7a377c309
Md. Nahid Hasan Limon <41167616+NahidHasanLimon@users.noreply.github.com>
2022-03-13 00:42:30 +0600

    custom middleware explored

--- express_exploring/app.js
+++ express_exploring/app.js
@@ -1,2 +1,46 @@
+
 const express = require('express');
-console.log(express);
+const app = express()
+
+const PORT = process.env.PORT || 8080
+const demoObj = {
+    topic: 'Node JS Learning',
+    startDate: Date(2022-03-07),
+    Purpose: 'Learning Mongo DB and node js and tends to nest js for microservice.',
+    Source: 'Stackschool.com',
+    personalDetails: {
+        name: 'Nahid Hasan Limon',
+        email: 'nh.limon2010@gmail.com',
+        phone: '01621316727'
+    },
+    modification: '2.0'
+}
+function customMiddleware(req,res,next){
+    console.log('Logged from my own middleware. '+ req.url)
+    if( req.url == '/block'){
+        res.send('<h1> Alert! You are accessing a blocked page. </h1>')
+    }
+    next()
+}
+function simpleLogger(){
+    return (req,res,next) => {
+        console.log(`${req.method} -- ${req.url}`)
+        next()
+    }
+}
+const myMiddleWares = [customMiddleware,simpleLogger()]
+app.use(myMiddleWares)
+app.get('/block',(req,res) => res.send ('<h2> Its a block page </h2>'))
+app.get('/contact',(req,res) => res.send('Thank you for contacting with us!'))
+app.get('/json', (req,res) => {
+    res.json(demoObj);
+})
+app.get('/', (req,res) => {
+    res.send('hey! Congrats! Route handler checked!');
+})
+app.listen(PORT, function(){
+    console.log(`server is running on ${PORT}`);
+})
+
+app.get('*',(req,res) => res.send('<h2> Route Not Found </h2>'))
+// console.log(express);
